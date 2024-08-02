6a9a2734ca75e4a5535625a4dfc1cf2787110473
sh-syl <142130386+sh-syl@users.noreply.github.com>
2024-08-02 12:09:30 +1000

    Update lcm.py

--- Question 1/lcm.py
+++ Question 1/lcm.py
@@ -1,16 +1,16 @@
-def lcm(x,y):
+def lcm(x, y):
     a = x
     b = y
     while y:
-        x,y = y,x % y
+        x, y = y, x % y
     return a * b // x
+
 
 while True:
     num = input('Numbers(Separate by space): ').split()
     LCM = int(num[0])
     for i in range(len(num)):
-        if i+1 == len(num):
+        if i + 1 == len(num):
             break
-        LCM = lcm(LCM,int(num[i+1]))
+        LCM = lcm(LCM, int(num[i + 1]))
     print(LCM)
-    
